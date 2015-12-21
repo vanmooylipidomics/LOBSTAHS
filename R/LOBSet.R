fddbb73ac840dbfaef33ccabfbb9cb5f6d5c0018
jamesrco <james.r.collins@aya.yale.edu>
2015-12-21 13:44:18 -0300

    Changed some S4 obj slot names for LOBSet

--- R/LOBSet.R
+++ R/LOBSet.R
@@ -3,33 +3,35 @@
 # create a class "LOBSet" for the results of LOBSTAHS screening & compound assignments
 
 LOBSet = setClass("LOBSet",
-                  representation(LOBpeakdata = "data.frame",
+                  representation(peakdata = "data.frame",
                                  iso.C3r = "list",
                                  iso.C3f = "list",
                                  iso.C3c = "list",
                                  LOBscreen.diagnostics = "data.frame",
                                  LOBisoID.diagnostics = "data.frame",
                                  LOBscreen.settings = "list",
-                                 parent.xsAnnotate = "xsAnnotate"
+                                 polarity = "factor",
+                                 sampnames = "character"
                   ),
                   
-                  prototype(LOBpeakdata = data.frame(),
+                  prototype(peakdata = data.frame(),
                             iso.C3r = list(),
                             iso.C3f = list(),
                             iso.C3c = list(),
                             LOBscreen.diagnostics = data.frame(),
                             LOBisoID.diagnostics = data.frame(),
                             LOBscreen.settings = list(),
-                            parent.xsAnnotate = new("xsAnnotate")
+                            polarity = factor(character(0)),
+                            sampnames = character(0)
                   ))
 
 setMethod("show", "LOBSet", function(object) {
   
-  cat("A",as.character(object@parent.xsAnnotate@polarity),"polarity \"LOBSet\" containing LC-MS peak data. Compound assignments and adduct ion hierarchy screening annotations applied to",length(sampnames(object@parent.xsAnnotate@xcmsSet)),"samples using the \"LOBSTAHS\" package.\n\n")
+  cat("A",as.character(object@polarity),"polarity \"LOBSet\" containing LC-MS peak data. Compound assignments and adduct ion hierarchy screening annotations applied to",length(object@sampnames),"samples using the \"LOBSTAHS\" package.\n\n")
   cat("Individual peaks:",object@LOBscreen.diagnostics$peaks[6],"\n")
   cat("Peak groups:",object@LOBscreen.diagnostics$peakgroups[6],"\n")
   cat("Compound assignments:",object@LOBscreen.diagnostics$parent_compounds[6],"\n")
-  cat("m/z range:",paste(min(object@LOBpeakdata$peakgroup.mz, na.rm = TRUE),max(object@LOBpeakdata$peakgroup.mz, na.rm = TRUE), sep = "-"),"\n\n")
+  cat("m/z range:",paste(min(object@peakdata$peakgroup.mz, na.rm = TRUE),max(object@peakdata$peakgroup.mz, na.rm = TRUE), sep = "-"),"\n\n")
   
   cat("Possible regisomers:",paste(object@LOBisoID.diagnostics$peakgroups[1],"\n"))
   cat("Possible structural functional isomers:",paste(object@LOBisoID.diagnostics$peakgroups[2],"\n"))
@@ -39,11 +41,9 @@ setMethod("show", "LOBSet", function(object) {
   
   cat("Match tolerance used for database assignments:",object@LOBscreen.settings$match.ppm,"ppm\n\n")
   #   cat("Ranges of chemical parameters represented in molecules other than pigments:\n\n")
-  #   cat("Total number of acyl carbon atoms:",paste(min(object@LOBpeakdata$FA_total_no_C, na.rm = TRUE),max(object@LOBpeakdata$FA_total_no_C, na.rm = TRUE), sep = "-"),"\n")
-  #   cat("Total number of acyl carbon-carbon double bonds:",paste(min(object@LOBpeakdata$FA_total_no_DB, na.rm = TRUE),max(object@LOBpeakdata$FA_total_no_DB, na.rm = TRUE), sep = "-"),"\n")
-  #   cat("Number of additional oxygen atoms:",paste(min(object@LOBpeakdata$degree_oxidation, na.rm = TRUE),max(object@LOBpeakdata$degree_oxidation, na.rm = TRUE), sep = "-"),"\n\n")
-  
-  cat("The CAMERA \"xsAnnotate\" object used for screening was retained in slot @parent.xsAnnotate.\n\n")
+  #   cat("Total number of acyl carbon atoms:",paste(min(object@peakdata$FA_total_no_C, na.rm = TRUE),max(object@peakdata$FA_total_no_C, na.rm = TRUE), sep = "-"),"\n")
+  #   cat("Total number of acyl carbon-carbon double bonds:",paste(min(object@peakdata$FA_total_no_DB, na.rm = TRUE),max(object@peakdata$FA_total_no_DB, na.rm = TRUE), sep = "-"),"\n")
+  #   cat("Number of additional oxygen atoms:",paste(min(object@peakdata$degree_oxidation, na.rm = TRUE),max(object@peakdata$degree_oxidation, na.rm = TRUE), sep = "-"),"\n\n")
   
   memsize = object.size(object)
   cat("Memory usage:", signif(memsize/2^20, 3), "MB\n")
