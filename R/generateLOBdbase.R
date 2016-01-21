c55c2c1a4495f6e98e1712c5f3f17f203893d609
jamesrco <james.r.collins@aya.yale.edu>
2016-01-21 09:24:06 -0500

    Scaled rounding down to 8 decimal places since this is limit of precision of our input mass data

--- R/generateLOBdbase.R
+++ R/generateLOBdbase.R
@@ -623,7 +623,7 @@ runSim = function(polarity, acylRanges, oxyRanges, adductHierarchies, baseCompon
   object = new("LOBdbase")
   
   object@frag_ID = as.integer(sim_results$frag_ID)
-  object@mz = round(as.numeric(sim_results$mz),12) # 12 decimal places is far more than enough precision for what we need; also, some very small arithmetic errors occur beyond the 14th decimal place which throw off distinction between isobars and true isomers
+  object@mz = round(as.numeric(sim_results$mz),8) # 8 decimal places is far more than enough precision for what we need; also, this is currently the limit of precision of our input exact mass data -- so we shouldn't exceed it. if we don't round here, we'll get some very small arithmetic errors beyond the 14th decimal place that will throw off distinction between isobars and true isomers
   object@exact_parent_neutral_mass = as.numeric(sim_results$exact_parent_neutral_mass)
   object@lipid_class = as.factor(as.character(sim_results$lipid_class))
   object@species = as.character(sim_results$species)
