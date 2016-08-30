2796d3945151c5164fb05100878a594932559f0e
Rico Derks <r.j.e.derks@lumc.nl>
2016-08-30 11:12:28 +0200

    Added loading user-specified retention time window data and check if file exists.

--- R/doLOBscreen.R
+++ R/doLOBscreen.R
@@ -170,6 +170,11 @@ doLOBscreen = function(xsA, polarity = NULL, database = NULL,
     } else { # load and perform basic check of user-specified rt window data, 
              # assuming they pointed rt.wintable to a valid R matrix with 
              # reasonbly named column headings
+      if (!file.exists(rt.windows)) {
+        stop("User defined file containing retention time window data does not exist!")
+      }
+      # load the retention time window data 
+      rt.windows = read.csv(rt.windows)
 
       if (sum(grepl("[Ll][Ii][Pp][Ii][Dd][ |\\.|_]*[Cc][Ll][Aa][Ss][Ss]",
                     colnames(rt.windows)))!=1) {
